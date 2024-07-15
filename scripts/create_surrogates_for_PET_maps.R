b7da76833cd2f3a1e720f56fc57a7c8826f32717
Christine Ecker <67059519+christineecker@users.noreply.github.com>
2024-07-15 17:25:36 +0200

    changed order

--- scripts/create_surrogates_for_PET_maps.R
+++ scripts/create_surrogates_for_PET_maps.R
@@ -1,23 +1,5 @@
 
 
-##| 5HTR1A --------------------------------------------------------
-##|
-
-fs.overlay <- c(pet.5HT$`5HT1A`$lh,
-                pet.5HT$`5HT1A`$rh)
-fs.overlay[fsavg6$medial.wall.verts$both] <- NA
-
-sa.5HT1A <- fs_create_surrogates(
-  fs.overlay = fs.overlay,
-  hemi = "both",
-  knn = c(30000L, 30000L),
-  n.surr = 1000L,
-  n.cores = 10L,
-  kernel = "gaussian"
-)
-#fs_plot(sa[sample(1:10,1),], "both")
-save(sa.5HT1A, file = "./output/5HT1A_Variograms/lh.rh.5HT1A.knn10000.sa1000.rda")
-
 ##| 5HTR2A --------------------------------------------------------
 ##|
 
@@ -71,3 +53,21 @@ sa.BZ <- fs_create_surrogates(
 )
 #fs_plot(sa[sample(1:10,1),], "both")
 save(sa.BZ, file = "./output/5HT4_Variograms/lh.rh.BZ.knn20000.sa1000.rda")
+
+##| 5HTR1A --------------------------------------------------------
+##|
+
+fs.overlay <- c(pet.5HT$`5HT1A`$lh,
+                pet.5HT$`5HT1A`$rh)
+fs.overlay[fsavg6$medial.wall.verts$both] <- NA
+
+sa.5HT1A <- fs_create_surrogates(
+  fs.overlay = fs.overlay,
+  hemi = "both",
+  knn = c(30000L, 30000L),
+  n.surr = 1000L,
+  n.cores = 10L,
+  kernel = "gaussian"
+)
+#fs_plot(sa[sample(1:10,1),], "both")
+save(sa.5HT1A, file = "./output/5HT1A_Variograms/lh.rh.5HT1A.knn10000.sa1000.rda")
